86f2dd46653d6ded990f661bf2eb64e9e5c7c6ee
BenjaminKovacs <bach1099-btkamazonwebservices@yahoo.com>
2018-09-21 20:22:21 -0400

    make it better

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
+const PORT = process.env.PORT || 5000
+
 app.get('/', function(req, res){
 	  	res.sendFile(path.join(__dirname, '/', 'client.html'));
 		});
+
 io.on('connection', function(socket){
   console.log('a user connected');
 }
-app.listen(8080, function(){
-	console.log('listening on *:8080');
+
+app.listen(PORT, function(){
+	console.log('listening on *:PORT');
 });
