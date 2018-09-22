3403ceaf0067e820deded43d64f29a7e3c316756
pi314mm <matias.scharager@gmail.com>
2018-09-21 21:16:53 -0400

    server stuff

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 var fs = require('fs');
 var http = require('http');
+var mysql = require('mysql');
+
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "user",
+  password: "password",
+  database: "stuff",
+  port: 3306
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
 
 http.createServer(function (req, res) {
     fs.readFile('client.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
+    con.query("SELECT * FROM items", function (err, result, fields) {
+		if (err) throw err;
+		console.log(result);
+	});
+	res.write(data);
     res.end();
   });
 }).listen(8080);
