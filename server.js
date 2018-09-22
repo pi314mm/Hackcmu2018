5e9a30119e0b5ea4ef0ae6d4c7f255cd5c03978d
BenjaminKovacs <bach1099-btkamazonwebservices@yahoo.com>
2018-09-21 20:08:36 -0400

    make it better

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-    //res.writeHead(200, {'Content-Type': 'text/html'});
-    //res.end('Hello World!');
-	app.get('/', function(req, res){
+"use strict";
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+app.get('/', function(req, res){
 	  	res.sendFile(path.join(__dirname, '/', 'client.html'));
 		});
-}).listen(8080);
+io.on('connection', function(socket){
+  console.log('a user connected');
+}
+app.listen(8080, function(){
+	console.log('listening on *:8080');
+});
