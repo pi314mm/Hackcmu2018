51abc8fdc584d9fc09c7d03d5a2ed99a9c6a4f9e
BenjaminKovacs <bach1099-btkamazonwebservices@yahoo.com>
2018-09-21 20:03:24 -0400

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
+    //res.writeHead(200, {'Content-Type': 'text/html'});
+    //res.end('Hello World!');
+	app.get('/', function(req, res){
+	  	res.sendFile(path.join(__dirname, '/', 'client.html'));
+		});
 }).listen(8080);
